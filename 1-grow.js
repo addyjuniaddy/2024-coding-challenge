09aeb83ac0e9e2c7ec88f727af77d3dc249e9abd
addyjuniaddy <addy.juniaddy@gmail.com>
2024-01-26 13:29:39 +0700

    Pakai perintah reduce

--- 1-grow.js
+++ 1-grow.js
@@ -1,8 +1,12 @@
-function grow(x) {
-  let result = x[0];
-  for(let i=1;i<x.length;i++){
-    result*=x[i];
-  }
-  return result;
+// function grow(x) {
+//   let result = x[0];
+//   for(let i=1;i<x.length;i++){
+//     result*=x[i];
+//   }
+//   return result;
+// }
+
+function grow(x){
+  return x.reduce((result, num) => result * num,1);
 }
 console.log(grow([2,3,2,3,4]));
