00b0c456904662bdb639d0f4abf0c4e8ab42f220
addyjuniaddy <addy.juniaddy@gmail.com>
2024-01-26 19:11:06 +0700

    Tambah 2 file python

---
+++ arraydiff.py
@@ -0,0 +1,9 @@
+def array_diff(a, b):
+    return [x for x in a if x not in b]
+
+# Example usage:
+result1 = array_diff([1, 2,13], [1])
+print(result1)  # Output: [2]
+
+result2 = array_diff([1, 2, 2, 2, 3], [2])
+print(result2)  # Output: [1, 3]
---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage:
+result1 = reverse_string('world is si dlrow')
+print(result1)  # Output: 'dlrow'
+
+result2 = reverse_string('word')
+print(result2)  # Output: 'drow'
